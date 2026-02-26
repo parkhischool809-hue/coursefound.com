f004a3afd7c7cb91ac9a2de66f1e13b772bbe42e
parkhischool809-hue <parkhischool809@gmail.com>
2026-02-26 12:15:17 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+        return f"Thank you {name}, we received your message!"
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
